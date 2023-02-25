3f369df5ed8885e47525955e3d489a69c79e522f
Thiago Colen <thiago.souzacolen@gmail.com>
2023-02-25 16:23:27 -0300

    remove old bin folder

--- bin/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _main = _interopRequireDefault(require("./main.js"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const main = new _main.default();
-main.init();
